f7d8040afb8644c92b7f219c6bc441e832d45ba7
hansamho <tkagh258@naver.com>
2024-02-19 09:03:01 +0900

    java_test

---
+++ src/Test/Test.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		int i =5;
+		if(i%2==0) {
+			System.out.println("짝수입니다");
+		}else {
+			System.out.println("홀수입니다");
+		}
+	}
+
+}
